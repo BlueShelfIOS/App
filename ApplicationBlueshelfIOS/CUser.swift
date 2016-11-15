71b45dbf71661efe9584f6d02271a00fe4b3142c
Maxime Dulin <maximedulin@Maximes-Mac.local>
2016-11-15 05:34:43 -0800

    Ajout de la classe User

---
+++ ApplicationBlueshelfIOS/CUser.swift
@@ -0,0 +1,13 @@
+//
+//  CUser.swift
+//  ApplicationBlueshelfIOS
+//
+//  Created by Maxime Dulin on 11/15/16.
+//  Copyright © 2016 Antoine Millet. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+}
